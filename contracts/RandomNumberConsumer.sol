0d7571511932a8c220c27d6e868e80268cc3cde2
MATTOP\Matthew <m.durkin92@gmail.com>
2021-09-02 18:17:48 +0100

    Add initial handling code for random number and assign fake 'tokens' to users

--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -5,10 +5,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RandomNumberConsumer is VRFConsumerBase, Ownable {
 
-    bytes32 internal keyHash;
-    uint256 internal fee;
+    bytes32 public keyHash;
+    uint256 public fee;
+    address public vrfCoordinator;
+    address public link;
     uint256 public randomResult;
+
+    mapping(bytes32 => address) public requestIdToAddress;
+    mapping(bytes32 => uint256) public requestIdToRandomNumber;
+
+    mapping(address => uint256) public addressToFakeChips;
+
     event RequestedRandomness(bytes32 requestId);
+    event SpinsOutcome(bytes32 requestId, address player, uint256 randomResult, uint256 payout1, uint256 payout2, uint256 payout3, uint256 payout4, uint256 payout5, uint256 payout6, uint256 payout7, uint256 payout8, uint256 payout9, uint256 payout10);
+
+    function linkBalance() external view returns (uint256 linkBalance_) {
+        linkBalance_ = LINK.balanceOf(address(this));
+    }
 
     constructor(address _vrfCoordinator,
                 address _link,
@@ -21,16 +34,26 @@ contract RandomNumberConsumer is VRFConsumerBase, Ownable {
     {
         keyHash = _keyHash;
         fee = _fee;
+        link = _link;
+        vrfCoordinator = _vrfCoordinator;
     }
 
     /** 
      * Requests randomness 
      */
-    function getRandomNumber() public returns (bytes32 requestId) {
+    function getRandomNumber() public payable returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-        requestId = requestRandomness(keyHash, fee);
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestIdToAddress[requestId] = msg.sender;
         emit RequestedRandomness(requestId);
-        return requestId;
+    }
+
+    function expand(uint256 randomValue, uint256 n) public pure returns (uint256[] memory expandedValues) {
+        expandedValues = new uint256[](n);
+        for (uint256 i = 0; i < n; i++) {
+            expandedValues[i] = uint256(keccak256(abi.encode(randomValue, i)));
+        }
+        return expandedValues;
     }
 
     /**
@@ -38,9 +61,44 @@ contract RandomNumberConsumer is VRFConsumerBase, Ownable {
      */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness;
+        address requestAddress = requestIdToAddress[requestId];
+        requestIdToRandomNumber[requestId] = randomness;
+        uint256[] memory expandedValues = expand(randomness, 10);
+        uint[] memory payouts = new uint[](10);
+        for (uint i=0; i<10; i++) {
+            uint256 spinValue = (expandedValues[i] % 1000) + 1;
+            if (spinValue <= 700) {
+                payouts[i] = 0;
+            } 
+            else if (spinValue > 700 && spinValue <= 850) {
+                payouts[i] = 10;
+            }
+            else if (spinValue > 850 && spinValue <= 950) {
+                payouts[i] = 20;
+            }
+            else if (spinValue > 950 && spinValue <= 1000) {
+                payouts[i] = 50;
+            }
+        }
+
+        uint totalPayout;
+         for (uint i=0; i<10; i++) {
+             totalPayout += payouts[i];
+        }
+
+        addressToFakeChips[requestAddress] += totalPayout;
+        emit SpinsOutcome(requestId, requestAddress, payouts[0], payouts[1], payouts[2], payouts[3], payouts[4], payouts[5], payouts[6], payouts[7], payouts[8], payouts[9], payouts[10]);
     }
 
     function withdrawLink() external onlyOwner {
        require(LINK.transfer(msg.sender, LINK.balanceOf(address(this))), "Unable to transfer");
+    }
+
+    function mintFakeChips() external {
+        addressToFakeChips[msg.sender] += 100;
+    }
+
+    function mintFakeChips(address to) external {
+        addressToFakeChips[to] += 100;
     }
 }
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -16,16 +16,16 @@ const networkConfig = {
     80001: {
         name: 'mumbai',
         linkToken: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
-        vrfCoordinator: '0x3d2341ADb2D31f1c5530cDC622016af293177AE0',
-        keyHash: '0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da',
+        vrfCoordinator: '0x8C7382F9D8f56b33781fE506E897a4F1e2d17255',
+        keyHash: '0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4',
         fee: '100000000000000',
         fundAmount: '1000000000000000'
     },
     137: {
         name: 'polygon',
         linkToken: '0xb0897686c545045aFc77CF20eC7A532E3120E0F1',
-        vrfCoordinator: '0x8C7382F9D8f56b33781fE506E897a4F1e2d17255',
-        keyHash: '0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4',
+        vrfCoordinator: '0x3d2341ADb2D31f1c5530cDC622016af293177AE0',
+        keyHash: '0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da',
         fee: '100000000000000',
         fundAmount: '1000000000000000'
     }
